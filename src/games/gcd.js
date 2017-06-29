19736dcbb7e1397fd3d336a252449788de3a3fce
nurgeld <nurgeldy@gmail.com>
2017-06-29 15:03:52 +0600

    some fix in gcd

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -5,19 +5,19 @@ const ruleForGcd = 'Find the greatest common divisor of given numbers.';
 
 const gcd = (a, b) => {
   if (!b) {
-    return Math.abs(a);
+    return a;
   }
   return gcd(b, a % b);
 };
 
-const firstNum = setRandNum(100);
-const secondNum = setRandNum(100);
-const question = `${firstNum} ${secondNum}`;
+// const firstNum = setRandNum(100);
+// const secondNum = setRandNum(100);
+// const question = `${firstNum} ${secondNum}`;
 
 const gcdGame = () => {
-  // const firstNum = setRandNum(100);
-  // const secondNum = setRandNum(100);
-  // const question = `${firstNum} ${secondNum}`;
+  const firstNum = setRandNum(100);
+  const secondNum = setRandNum(100);
+  const question = `${firstNum} ${secondNum}`;
   const answer = gcd(firstNum, secondNum);
   return cons(question, answer);
 };
