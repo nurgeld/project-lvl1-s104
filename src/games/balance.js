7107b98b34e0c901b2bf747abacfd3d14dea5215
nurgeld <nurgeldy@gmail.com>
2017-07-01 23:16:10 +0600

    trying fix bugs in logic and code of balance func

--- src/games/balance.js
+++ src/games/balance.js
@@ -5,27 +5,27 @@ import getRandNum from '../randomizer';
 const ruleForBalance = 'Balance the given number.';
 
 const balance = (number) => {
-  // const strNum = String(number);
-  let result = '';
-  const iter = (num, acc) => {
-    const strNum = String(num);
-    if (acc === strNum.length - 1) {
-      return result;
-    }
-    if (Number(strNum[acc]) - Number(strNum[acc + 1]) > 1) {
-      result += Number(strNum[acc]) - 1;
-      result += Number(strNum[acc + 1]) + 1;
-      result += strNum.substring(acc + 2);
-    }
-    if (Number(strNum[acc]) - Number(strNum[acc + 1]) < -1) {
-      result += Number(strNum[acc]) + 1;
-      result += Number(strNum[acc + 1]) - 1;
-      result += strNum.substring(acc + 2);
-    }
-    result = strNum;
-    return iter(result, acc + 1);
-  };
-  return iter(number, 0);
+  //
+  // let result = '';
+  const strNum = String(number);
+  const array = [];
+  for (let i = 0; i < strNum.length; i += 1) {
+    array.push(Number(strNum[i]));
+  }
+  const max = Math.max(...array); // прочитал на https://medium.com/@vladbezden/how-to-get-min-or-max-of-an-array-in-javascript-1c264ec6e1aa
+  const min = Math.min(...array);
+  //
+  if (max - min > 1) {
+    array[array.indexOf(max)] = max - 1;
+    array[array.indexOf(min)] = min + 1;
+  }
+  const appendCurrent = (previousValue, currentValue) => previousValue + currentValue;
+  const result = array.reduce(appendCurrent); // посмотрел по ссылке https://msdn.microsoft.com/ru-ru/library/ff679975(v=vs.94).aspx
+  const resultNum = Number(result);
+  if (max - min <= 1) {
+    return resultNum;
+  }
+  return balance(resultNum);
 };
 
 export const balanceGame = () => {
