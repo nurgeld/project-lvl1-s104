db7b9ca2ad9afef8a1f2fb17bfe1581c61d24443
nurgeld <nurgeldy@gmail.com>
2017-07-01 15:48:32 +0600

    trying iterative process in balance func

--- src/games/balance.js
+++ src/games/balance.js
@@ -5,27 +5,27 @@ import getRandNum from '../randomizer';
 const ruleForBalance = 'Balance the given number.';
 
 const balance = (number) => {
-  const strNum = String(number);
+  // const strNum = String(number);
   let result = '';
-  for (let i = 0; i < strNum.length - 1; i += 1) {
-    // let result = '';
-    if (Number(strNum[i]) - Number(strNum[i + 1]) > 1) {
-      result += Number(strNum[i]) - 1;
-      result += Number(strNum[i + 1]) + 1;
-      result += strNum.substring(i + 2, strNum.length);
+  const iter = (num, acc) => {
+    const strNum = String(num);
+    if (acc === strNum.length - 1) {
+      return result;
     }
-    if (Number(strNum[i]) - Number(strNum[i + 1]) < -1) {
-      result += Number(strNum[i]) + 1;
-      result += Number(strNum[i + 1]) - 1;
-      result += strNum.substring(i + 2, strNum.length);
+    if (Number(strNum[acc]) - Number(strNum[acc + 1]) > 1) {
+      result += Number(strNum[acc]) - 1;
+      result += Number(strNum[acc + 1]) + 1;
+      result += strNum.substring(acc + 2, strNum.length);
     }
-    if (Math.abs(Number(strNum[i]) - Number(strNum[i + 1])) <= 1) {
-      result += strNum;
+    if (Number(strNum[acc]) - Number(strNum[acc + 1]) < -1) {
+      result += Number(strNum[acc]) + 1;
+      result += Number(strNum[acc + 1]) - 1;
+      result += strNum.substring(acc + 2, strNum.length);
     }
-  }
-  // надо как-то выйти из рекурсии, с результатом, имеющим сбалансированное
-  // число (на этом этапе с еще не упорядоченными цифрами)
-  return balance(Number(result));
+    result += strNum;
+    return iter(result, acc + 1);
+  };
+  return iter(number, 0);
 };
 
 export const balanceGame = () => {
