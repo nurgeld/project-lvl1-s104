301b3faac8dead37094ec6018004ec669ac41087
nurgeld <nurgeldy@gmail.com>
2017-07-02 13:41:42 +0600

    bug fix in balance game

--- src/games/balance.js
+++ src/games/balance.js
@@ -5,27 +5,25 @@ import getRandNum from '../randomizer';
 const ruleForBalance = 'Balance the given number.';
 
 const balance = (number) => {
-  //
-  // let result = '';
   const strNum = String(number);
   const array = [];
+  let str = '';
   for (let i = 0; i < strNum.length; i += 1) {
-    array.push(Number(strNum[i]));
+    array[i] = Number(strNum[i]);
   }
-  const max = Math.max(...array); // прочитал на https://medium.com/@vladbezden/how-to-get-min-or-max-of-an-array-in-javascript-1c264ec6e1aa
+  const max = Math.max(...array);
   const min = Math.min(...array);
-  //
-  if (max - min > 1) {
-    array[array.indexOf(max)] = max - 1;
-    array[array.indexOf(min)] = min + 1;
-  }
-  const appendCurrent = (previousValue, currentValue) => previousValue + currentValue;
-  const result = array.reduce(appendCurrent); // посмотрел по ссылке https://msdn.microsoft.com/ru-ru/library/ff679975(v=vs.94).aspx
-  const resultNum = Number(result);
   if (max - min <= 1) {
-    return resultNum;
+    return number;
   }
-  return balance(resultNum);
+  array[array.indexOf(max)] = max - 1;
+  array[array.indexOf(min)] = min + 1;
+  array.sort();
+  for (let i = 0; i < strNum.length; i += 1) {
+    str += array[i];
+  }
+  const newNum = Number(str);
+  return balance(newNum);
 };
 
 export const balanceGame = () => {
