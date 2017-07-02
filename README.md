7d2d21b2c29a8d11358a18edd0f8e30529cb18ce
nurgeld <nurgeldy@gmail.com>
2017-07-03 00:23:22 +0600

    refactoring generateProgression func

--- README.md
+++ README.md
@@ -1,3 +1,25 @@
-# project-lvl1-s104
+# Brain-games - project lvl1 by Hexlet
 [![Code Climate](https://codeclimate.com/github/codeclimate/codeclimate/badges/gpa.svg)](https://github.com/nurgeld/project-lvl1-s104)
 [![Build Status](https://travis-ci.org/nurgeld/project-lvl1-s104.svg?branch=master)](https://travis-ci.org/nurgeld/project-lvl1-s104)
+
+## Install
+
+```
+npm i -g braingames-104-nd
+```
+
+## play
+
+```
+brain-even
+
+brain-calc
+
+brain-gcd
+
+brain-prime
+
+brain-progression
+
+brain-balance
+```
--- src/games/progression.js
+++ src/games/progression.js
@@ -4,23 +4,22 @@ import getRandNum from '../randomizer';
 
 const ruleForProgression = 'What number is missing in this progression?';
 
-const progression = () => {
+const generateProgression = () => {
   const a1 = getRandNum(10) + 5;
   const d = getRandNum(10) + 1;
   const n = 10;
-  const randIndex = getRandNum(10);
-  const array = [];
+  const randIndex = getRandNum(10) + 1;
   let str = '';
   for (let i = 1; i <= n; i += 1) {
-    array[i - 1] = a1 + ((i - 1) * d);
-  }
-  const answer = array[randIndex];
-  array[array.indexOf(answer)] = '..';
-  for (let i = 0; i < n; i += 1) {
-    str += `${array[i]} `;
+    if (i === randIndex) {
+      str += '.. ';
+    } else {
+      str += `${a1 + ((i - 1) * d)} `;
+    }
   }
   const question = str;
+  const answer = a1 + ((randIndex - 1) * d);
   return cons(question, answer);
 };
 
-export default () => gameLogic(cons(ruleForProgression, progression));
+export default () => gameLogic(cons(ruleForProgression, generateProgression));
