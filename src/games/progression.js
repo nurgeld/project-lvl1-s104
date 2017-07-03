d1e9482c2de8f1037e236a96c2edd5a313c17a46
nurgeld <nurgeldy@gmail.com>
2017-07-03 20:33:45 +0600

    refactoring generateProgression func

--- src/games/progression.js
+++ src/games/progression.js
@@ -4,22 +4,19 @@ import getRandNum from '../randomizer';
 
 const ruleForProgression = 'What number is missing in this progression?';
 
-const generateProgression = () => {
+const elementOfProgression = (a1, d, n) => a1 + ((n - 1) * d);
+
+const progressionGame = () => {
   const a1 = getRandNum(10) + 5;
   const d = getRandNum(10) + 1;
   const n = 10;
   const randIndex = getRandNum(10) + 1;
-  let str = '';
+  let question = '';
   for (let i = 1; i <= n; i += 1) {
-    if (i === randIndex) {
-      str += '.. ';
-    } else {
-      str += `${a1 + ((i - 1) * d)} `;
-    }
+    question += (i === randIndex) ? '.. ' : `${elementOfProgression(a1, d, i)} `;
   }
-  const question = str;
-  const answer = a1 + ((randIndex - 1) * d);
+  const answer = elementOfProgression(a1, d, randIndex);
   return cons(question, answer);
 };
 
-export default () => gameLogic(cons(ruleForProgression, generateProgression));
+export default () => gameLogic(cons(ruleForProgression, progressionGame));
